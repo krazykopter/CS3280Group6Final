bfcde0da3a551fb486ebb3d1f82b6b4857df9e25
michaelcraigWeber <47675916+michaelcraigWeber@users.noreply.github.com>
2019-04-06 00:18:43 -0600

    HandleError as a class
    I propose storing the HandleError method as a class rather than implementing three copies of the same code into each xaml.cs file. I have not yet seen HandleError used outside of wndMain.xaml.cs, but I believe it will be necessary for all top-level methods (such as the clicks). Let me know what you think.
    
    -MWC

---
+++ Group6FinalProject/Group6FinalProject/clsHandleError.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Group6FinalProject
+{
+    public class clsHandleError
+    {
+        /// <summary>
+        /// This method handles all exceptions that have risen from lower level methods
+        /// </summary>
+        /// <param name="sClass"> The class where the error occurred </param>
+        /// <param name="sMethod"> The method where the error occurred </param>
+        /// <param name="sMessage"> The error message from the exception </param>
+        public void HandleError(string sClass, string sMethod, string sMessage)
+        {
+            try
+            {
+                MessageBox.Show(sClass + "." + sMethod + " -> " + sMessage);
+            }
+            catch (System.Exception ex)
+            {
+                System.IO.File.AppendAllText(@"C:\Error.txt", Environment.NewLine + "HandleError Exception: " + ex.Message);
+            }
+        }
+    }
+}
